d622eef1e661658fbb1d89b6fe32d0dbed552e8e
jwong <jeffrey.wong@mycom-osi.com>
2020-11-09 08:22:07 +0000

    basic auth

--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-
+const Leaders = require('../models/leaders');
 
 const leaderRouter = express.Router();
 
@@ -9,39 +10,71 @@ leaderRouter.use(bodyParser.json());
 
 
 leaderRouter.route('/')
-.all((req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
 .get((req,res,next) => {
-   res.end('will send all the leaders to you!'); 
+    Leaders.find({}).then((leaders) => {
+        res.statusCode  = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(leaders);
+    }, (err) => next(err))
+    .catch((err) => next(err))
 })
 .post((req,res,next) => {
-    res.end('Will add the leader: ' + req.body.name +  'with details: ' + req.body.description);
+    Leaders.create(req.body)
+    .then((leader) => {
+        console.log('leader created', leader);
+        res.statusCode  = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(leader);
+    }, (err) => next(err))
+    .catch((err) => next(err))
 })
 .put((req,res,next) => {
     res.statusCode = 403;
     res.end('PUT option not supported on /leaders');
 })
 .delete((req,res,next) => {
-    res.end('Deleting all the leaders!');
+    Leaders.remove({})
+    .then((resp) => {
+        res.statusCode  = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err))
 });
 
 leaderRouter.route('/:leaderId')
 .get((req,res,next) => {
-    res.end('will send details of the leaders: ' + req.params.leaderId + ' to you');
+    Leaders.findById(req.params.leaderId)
+    .then((leader) => {
+        res.statusCode  = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(leader);
+    }, (err) => next(err))
+    .catch((err) => next(err))
 })
 .post((req,res,next) => {
     res.statusCode = 403;
     res.end('POST option not supported on /leaders/' + req.params.leaderId);
 })
 .put((req,res,next) => {
-    res.end('Updating the leader: ' + req.params.leaderId + '\n');
-    res.end('Will update the leader: ' + req.body.name + ' with details: ' + req.body.description);
+    Leaders.findByIdAndUpdate(req.params.leaderId, {
+        $set: req.body
+    }, { new: true })
+    .then((resp) => {
+        res.statusCode  = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err))
 })
 .delete((req,res,next) => {
-    res.end('Deleting leader: ' + req.params.leaderId);
+    Leaders.findByIdAndRemove(req.params.leaderId)
+    .then((resp) => {
+        res.statusCode  = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err))
 });
      
 
--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+const Promotions = require('../models/promotions');
 
 
 const promoRouter = express.Router();
@@ -9,39 +11,71 @@ promoRouter.use(bodyParser.json());
 
 
 promoRouter.route('/')
-.all((req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
 .get((req,res,next) => {
-   res.end('will send all the promos to you!'); 
+    Promotions.find({}).then((promotions) => {
+        res.statusCode  = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotions);
+    }, (err) => next(err))
+    .catch((err) => next(err))
 })
 .post((req,res,next) => {
-    res.end('Will add the promo: ' + req.body.name +  'with details: ' + req.body.description);
+    Promotions.create(req.body)
+    .then((promotion) => {
+        console.log('promotion created', promotion);
+        res.statusCode  = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
+    }, (err) => next(err))
+    .catch((err) => next(err))
 })
 .put((req,res,next) => {
     res.statusCode = 403;
     res.end('PUT option not supported on /promotions');
 })
 .delete((req,res,next) => {
-    res.end('Deleting all the promotions!');
+    Promotions.remove({})
+    .then((resp) => {
+        res.statusCode  = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err))
 });
 
 promoRouter.route('/:promoId')
 .get((req,res,next) => {
-    res.end('will send details of the promotions: ' + req.params.promoId + ' to you');
+    Promotions.findById(req.params.promoId)
+    .then((promotion) => {
+        res.statusCode  = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promotion);
+    }, (err) => next(err))
+    .catch((err) => next(err))
 })
 .post((req,res,next) => {
     res.statusCode = 403;
     res.end('POST option not supported on /promotions/' + req.params.promoId);
 })
 .put((req,res,next) => {
-    res.end('Updating the promo: ' + req.params.promoId + '\n');
-    res.end('Will update the promo: ' + req.body.name + ' with details: ' + req.body.description);
+    Promotions.findByIdAndUpdate(req.params.promoId, {
+        $set: req.body
+    }, { new: true })
+    .then((resp) => {
+        res.statusCode  = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err))
 })
 .delete((req,res,next) => {
-    res.end('Deleting promo: ' + req.params.promoId);
+    Promotions.findByIdAndRemove(req.params.promoId)
+    .then((resp) => {
+        res.statusCode  = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err))
 });
      
 
