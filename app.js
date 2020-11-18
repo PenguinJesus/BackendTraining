e7fea2ca512c45d47c776a4c5beb5959d8c7ad4d
jwong <jeffrey.wong@mycom-osi.com>
2020-11-18 08:24:07 +0000

    passport-jwt

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var passport = require('passport');
-var authenticate = require('./authenticate');
+var config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,7 +18,7 @@ const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
@@ -37,32 +37,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('secretKey'));
 
-app.use(session({
-  name: 'session-id',
-  secret: 'sessionSecret',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
 
 app.use(passport.initialize());
-app.use(passport.session());
 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function auth(req,res, next){
-  if(!req.user){
-    var err = new Error('You are not authenticated');
-    err.status = 403;
-    next(err);
-  } else {
-    next();
-  }
-}
-
-app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const authenticate = require('../authenticate');
 
 const Dishes = require('../models/dishes');
 
@@ -20,7 +21,7 @@ dishRouter.route('/')
    }, (err) => next(err))
    .catch((err) => next(err))
 })
-.post((req,res,next) => {
+.post(authenticate.verifyUser,(req,res,next) => {
     Dishes.create(req.body)
     .then((dish) => {
         console.log('Dish created', dish);
@@ -30,11 +31,11 @@ dishRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err))
 })
-.put((req,res,next) => {
+.put(authenticate.verifyUser,(req,res,next) => {
     res.statusCode = 403;
     res.end('PUT option not supported on /dishes');
 })
-.delete((req,res,next) => {
+.delete(authenticate.verifyUser,(req,res,next) => {
     Dishes.remove({})
     .then((resp) => {
         res.statusCode  = 200;
@@ -54,11 +55,11 @@ dishRouter.route('/:dishId')
     }, (err) => next(err))
     .catch((err) => next(err))
 })
-.post((req,res,next) => {
+.post(authenticate.verifyUser,(req,res,next) => {
     res.statusCode = 403;
     res.end('POST option not supported on /dishes/' + req.params.dishId);
 })
-.put((req,res,next) => {
+.put(authenticate.verifyUser,(req,res,next) => {
     Dishes.findByIdAndUpdate(req.params.dishId, {
         $set: req.body
     }, { new: true })
@@ -69,7 +70,7 @@ dishRouter.route('/:dishId')
     }, (err) => next(err))
     .catch((err) => next(err))
 })
-.delete((req,res,next) => {
+.delete(authenticate.verifyUser,(req,res,next) => {
     Dishes.findByIdAndRemove(req.params.dishId)
     .then((resp) => {
         res.statusCode  = 200;
@@ -99,7 +100,7 @@ dishRouter.route('/:dishId/comments')
    }, (err) => next(err))
    .catch((err) => next(err))
 })
-.post((req,res,next) => {
+.post(authenticate.verifyUser,(req,res,next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
         if (dish != null){
@@ -118,11 +119,11 @@ dishRouter.route('/:dishId/comments')
     }, (err) => next(err))
     .catch((err) => next(err))
 })
-.put((req,res,next) => {
+.put(authenticate.verifyUser,(req,res,next) => {
     res.statusCode = 403;
     res.end('PUT option not supported on /dishes/' + req.params.dishId + '/comments' );
 })
-.delete((req,res,next) => {
+.delete(authenticate.verifyUser,(req,res,next) => {
     Dishes.findById(req.params.dishId)
     .then((dish) => {
         if (dish != null){
