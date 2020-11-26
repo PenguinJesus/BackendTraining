dfd2147e04a7f4083fd0fac9c3981ef7f69272a6
jwong <jeffrey.wong@mycom-osi.com>
2020-11-26 18:10:05 +0000

    Passport facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': 'secretKey',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '287605476004356',
+        clientSecret: '9a7202af9fd2a9ebe0f3c18173b37fce'
+    }
 }
