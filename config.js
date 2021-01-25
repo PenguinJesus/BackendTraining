d354ebaaf6a770efb5f97469b6e6d1e4de565381
PenguinJesus <67099286+PenguinJesus@users.noreply.github.com>
2021-01-25 09:53:02 +0000

    Update config.js
    removing credentials

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': 'secretKey',
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '287605476004356',
-        clientSecret: '9a7202af9fd2a9ebe0f3c18173b37fce'
+        clientId: '******************',
+        clientSecret: '**********************'
     }
 }
